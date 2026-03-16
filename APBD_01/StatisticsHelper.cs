2222920da410d8ca09ae7e8dfe7a4c8c5c22bed7
Igor Pytlinski <s34292@pjwstk.edu.pl>
2026-03-16 21:18:07 +0100

    Branch2 commit

--- APBD_01/StatisticsHelper.cs
+++ APBD_01/StatisticsHelper.cs
@@ -8,6 +8,9 @@ namespace APBD_01
         {
             if (values == null || values.Length == 0) return 0;
             return values.Average();
+            double sum = 0;
+            foreach (int i in values) sum += i;
+            return sum / values.Length;
         }
     }
 }
