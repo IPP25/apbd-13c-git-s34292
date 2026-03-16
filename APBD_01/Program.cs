3a27f1538939304af0976a48e8e68aaac403735c
Igor Pytlinski <s34292@pjwstk.edu.pl>
2026-03-16 21:02:18 +0100

    Adding user input

--- APBD_01/Program.cs
+++ APBD_01/Program.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace APBD_01
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
         }
     }
 }
