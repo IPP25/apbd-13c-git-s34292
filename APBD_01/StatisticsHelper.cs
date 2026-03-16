80c34f6a06becd95a36332a971c7b252f2f823de
Igor Pytlinski <s34292@pjwstk.edu.pl>
2026-03-16 21:26:04 +0100

    Add Calulate Max

--- APBD_01/StatisticsHelper.cs
+++ APBD_01/StatisticsHelper.cs
@@ -12,5 +12,17 @@ namespace APBD_01
             foreach (int i in values) sum += i;
             return sum / values.Length;
         }
+        public int CalculateMax(int[] values)
+        {
+            if (values == null || values.Length == 0) return int.MinValue; 
+            int max = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > max) max = values[i];
+            }
+            return max;
+            
+        }
+        
     }
 }
